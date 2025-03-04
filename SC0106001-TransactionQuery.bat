c5e7bf129bad10e5ded923ef2a13ca376ad31897
Jenny <tuluber@gmail.com>
2025-03-04 14:23:01 +0800

    加入起迄時間及總執行時間(毫秒)

--- SC0106001-TransactionQuery.bat
+++ SC0106001-TransactionQuery.bat
@@ -2,6 +2,11 @@
 chcp 65001 >nul
 echo ● [START] %~nx0
 
+:: 記錄發送請求的時間
+for /f "delims=" %%a in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss.fff'"') do set start_time=%%a
+echo 發送請求時間: %start_time%
+
+
 if exist cid.txt (	:: 讀取 CID（如果檔案存在）
     set /p CID=<cid.txt
 )
@@ -14,4 +19,12 @@ curl -X GET "http://127.0.0.1:%2/%1/%3?cid=%CID%&cardType=2&startDate=2024/01/01
 
 
 echo.
+:: 記錄請求完成的時間
+for /f "delims=" %%a in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss.fff'"') do set end_time=%%a
+echo 完成請求時間: %end_time%
+
+:: 計算執行時間（毫秒）
+for /f "delims=" %%a in ('powershell -Command "(Get-Date).Subtract([datetime]'%start_time%').TotalMilliseconds"') do set duration=%%a
+echo ○ 總執行時間: %duration% 毫秒
+
 echo ● [END] %~nx0
