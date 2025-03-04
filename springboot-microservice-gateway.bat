395fc4da0b0779e533f5b22f1b5edb784247ce43
Jenny <tuluber@gmail.com>
2025-03-04 13:42:48 +0800

    springboot-microservice-gateway.bat test OK

---
+++ springboot-microservice-gateway.bat
@@ -0,0 +1,86 @@
+@echo off
+chcp 65001 >nul
+echo [START] %~nx0
+echo ==========
+echo.
+
+
+
+setlocal enabledelayedexpansion
+
+:: 取得批次檔名稱（不含副檔名）
+set "BATCH_NAME=%~n0"
+
+:: docker container port號
+set "PORT=8095"
+
+:: URL 路徑
+set "SC0101001-Application=management/Application"
+set "SC0102001-Review=management/Review"
+set "SC0103001-Activation=management/Activation"
+set "SC0104001-Transaction=transactions/Transaction"
+set "SC0106001-TransactionQuery=transactions/TransactionQuery"
+set "SC0106002-DisputeNotation=dispute/DisputeNotation"
+set "SC0107001-FeePayment=billing/FeePayment"
+
+:: 定義大寫英文字母
+set "LETTERS=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+:: 產生首字（大寫字母）
+set /a randIndex=%random% %% 26
+set "CID=!LETTERS:~%randIndex%,1!"
+
+:: 產生後續 9 碼（0-9 數字）
+for /l %%i in (1,1,9) do (
+    set /a randNum=!random! %% 10
+    set "CID=!CID!!randNum!"
+)
+
+:: 將身分證號儲存為檔案
+<nul set /p ="!CID!" >cid.txt
+
+
+:: 申請信用卡
+call SC0101001-Application.bat %BATCH_NAME% %PORT% %SC0101001-Application%
+echo ----------
+echo.
+
+:: 銀行審核信用卡
+call SC0102001-Review.bat %BATCH_NAME% %PORT% %SC0102001-Review%
+echo ----------
+echo.
+
+:: 客戶信用卡開卡
+call SC0103001-Activation.bat %BATCH_NAME% %PORT% %SC0103001-Activation%
+echo ----------
+echo.
+
+:: 客戶信用卡消費
+call SC0104001-Transaction.bat %BATCH_NAME% %PORT% %SC0104001-Transaction%
+echo ----------
+echo.
+
+:: 消費紀錄區間查詢
+call SC0106001-TransactionQuery.bat %BATCH_NAME% %PORT% %SC0106001-TransactionQuery%
+echo ----------
+echo.
+
+:: 客戶爭議款項申請
+call SC0106002-DisputeNotation.bat %BATCH_NAME% %PORT% %SC0106002-DisputeNotation%
+echo ----------
+echo.
+
+:: 客戶繳交信用卡費
+call SC0107001-FeePayment.bat %BATCH_NAME% %PORT% %SC0107001-FeePayment%
+echo ----------
+echo.
+
+
+
+
+
+echo ==========
+echo [END] %~nx0
+
+pause
+
